4087f681d0affaf26fceb7dfdd283da4c5c68361
shubhamxdd <ssisodia128@gmail.com>
2024-11-08 13:55:50 +0530

    create register view and route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,47 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
+const userModel = require("../models/user.js");
+
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("hello from /user/");
+// register
+router.get("/register", async (req, res) => {
+  res.render("register");
 });
+
+router.post(
+  "/register",
+  body("email").trim().isEmail(),
+  body("password").trim().isLength({ min: 3 }),
+  body("username").trim().isLength({ min: 3 }),
+  async (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    // console.log(errors);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        errors: errors.array(),
+        message: "Invalid Data.",
+      });
+    }
+
+    const { email, username, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    res.json(user);
+  }
+);
+
+// TODO: login
 
 module.exports = router;
